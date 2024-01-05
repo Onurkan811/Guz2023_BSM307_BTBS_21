42a42192bedd80877672619d84ad7da175e97550
Onurkan <onurkan_2013@hotmail.com>
2024-01-05 21:38:20 +0300

    .

--- CHAswifT/client/src/App.jsx
+++ CHAswifT/client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL= import.meta.env.CLIENT_URL
+  axios.defaults.baseURL= "https://guz2023-bsm-307-btbs-21-api.vercel.app"
   axios.defaults.withCredentials= true;
   return (
     <UserContextProvider>
