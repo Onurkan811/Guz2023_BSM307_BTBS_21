500b598d045b6e0d333371e4f94499e9c5e91e41
Onurkan <onurkan_2013@hotmail.com>
2024-01-05 21:58:29 +0300

    websocket fix 3

--- CHAswifT/client/src/chat.jsx
+++ CHAswifT/client/src/chat.jsx
@@ -22,7 +22,7 @@ export default function Chat(){
     },[selectedUserId]);
 
     function connectToWs() {
-        const ws = new WebSocket('ws://localhost:4040');
+        const ws = new WebSocket('wss://guz2023-bsm-307-btbs-21-api.vercel.app');
         setWs(ws);
         ws.addEventListener('message', handleMessage);
         ws.addEventListener('close', () => {
